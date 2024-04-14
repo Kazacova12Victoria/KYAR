8359fc16487e221fd1014ad9cae4b33debb51fab
Kazacova12Victoria <vk200640@gmail.com>
2024-04-14 19:24:34 +0300

    .

--- js/forMainPage.js
+++ js/forMainPage.js
@@ -1,4 +1,6 @@
 window.addEventListener('DOMContentLoaded', () => {
+
+    let array = [];
     fetch('js/cd_catalog.xml')
         .then(function (response) {
             return response.text();
@@ -8,23 +10,34 @@ window.addEventListener('DOMContentLoaded', () => {
             const xmlDoc = parser.parseFromString(xmlDocText, "text/xml");
             const MenuCards = xmlDoc.querySelectorAll('text');
             const tableBody = document.querySelector(".description1");
-            console.log(MenuCards);
             MenuCards.forEach(function (student) {
                 const scr = student.querySelector("imagine").textContent;
                 const title = student.querySelector("title").textContent;
                 const text = student.querySelector("description").textContent;
                 const linkHref = student.querySelector('linkhref').textContent;
-                tableBody.innerHTML += `
-                <img src="${scr}" />
-                <div class="txt">
-                    <h2>${title}</h2>
-                    <br>
-                    <p>${text}</p>
-                        <br>
-                        <a class="link1" href="${linkHref}">Читать подробнее о туре</a>
-                </div>
-                `;
+                const price = student.querySelector('price').textContent;
+                array.push({scr, title, text, linkHref, price});
             });
+            array.sort(function(a, b) {
+                return a.price - b.price;
+              });
+            
+        for(let i = 0; i < array.length-1; i++){
+            tableBody.innerHTML += `
+            <img src="${array[i].scr}" />
+            <div class="txt">
+                <h2>${array[i].title}</h2>
+                <br>
+                <p>${array[i].text}</p>
+                    <br>
+                    <div class="descr__container" style="display: flex; justify-content: space-between; padding-right: 20px">
+                    <a class="link1" href="${array[i].linkHref}">Читать подробнее о туре</a>
+                    <div class="price">${array[i].price}</div>
+                    </div>
+            </div>
+            `;  
+        }
         });
-        
+        console.log(array);
+    
 });

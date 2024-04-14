20cffc6c624b711406fd1efbcbdc1f2ad9b6b6d4
Kazacova12Victoria <vk200640@gmail.com>
2024-04-14 17:35:57 +0300

    /

--- js/forMainPage.js
+++ js/forMainPage.js
@@ -7,20 +7,6 @@ window.addEventListener('DOMContentLoaded', () => {
     xmlhttp.send();
     const xmlDoc = xmlhttp.responseXML;
 
-    console.dir(xmlDoc.getElementsByTagName("title"));
-    document.querySelector('.description1').innerHTML = `
-    <img src="photo/Египет.webp">
-        <div class="txt">
-            <h2>Египет</h2>
-            <br>
-            <p>Исследуйте древний мир Египта за один день! Вас ждут удивительные встречи с сфинксом, прогулка по древней крепости, открытие сокровищ
-                 Тутанхамона, увлекательные истории о пирамидах . Проведите незабываемый день в самом большом городе Египта, полный загадок и 
-                 удивительных открытий.</p>
-                <br>
-                 <a class="link1" href="page5.html">Читать подробнее о туре</a>
-        </div>
-    `;
-
     fetch('js/cd_catalog.xml')
         .then(function (response) {
             return response.text();

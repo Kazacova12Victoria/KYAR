0e712e99c01d951ced546824f52aa048e5ddaf5d
Kazacova12Victoria <vk200640@gmail.com>
2024-04-14 17:31:33 +0300

    .

--- js/forMainPage.js
+++ js/forMainPage.js
@@ -3,7 +3,7 @@ window.addEventListener('DOMContentLoaded', () => {
     const parent = document.querySelector('.description1');
 
     xmlhttp = new XMLHttpRequest();
-    xmlhttp.open("GET","cd_catalog.xml",false);
+    xmlhttp.open("GET","js/cd_catalog.xml",false);
     xmlhttp.send();
     const xmlDoc = xmlhttp.responseXML;
 
@@ -19,5 +19,34 @@ window.addEventListener('DOMContentLoaded', () => {
                 <br>
                  <a class="link1" href="page5.html">Читать подробнее о туре</a>
         </div>
-    `
+    `;
+
+    fetch('js/cd_catalog.xml')
+        .then(function (response) {
+            return response.text();
+        })
+        .then(function (xmlDocText) {
+            const parser = new DOMParser();
+            const xmlDoc = parser.parseFromString(xmlDocText, "text/xml");
+            const MenuCards = xmlDoc.querySelectorAll('text');
+            const tableBody = document.querySelector(".description1");
+            console.log(MenuCards);
+            MenuCards.forEach(function (student) {
+                const scr = student.querySelector("imagine").textContent;
+                const title = student.querySelector("title").textContent;
+                const text = student.querySelector("description").textContent;
+                const linkHref = student.querySelector('linkhref').textContent;
+                tableBody.innerHTML += `
+                <img src="${scr}" />
+                <div class="txt">
+                    <h2>${title}</h2>
+                    <br>
+                    <p>${text}</p>
+                        <br>
+                        <a class="link1" href="${linkHref}">Читать подробнее о туре</a>
+                </div>
+                `;
+            });
+        })
+
 });
